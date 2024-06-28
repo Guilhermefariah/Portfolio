a4e67c04b5ac1a1d335e55b50c45e7bac08d7fd9
Guilhermefariah <guilhermefaria321@gmail.com>
2024-06-28 15:18:52 -0300

    hover text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,14 @@ const Header: React.FC = () => {
       <h1 className="text-3xl font-bold">Meu Portfólio</h1>
       <nav>
         <ul className="flex space-x-4">
-          <li><a href="#about">Sobre</a></li>
-          <li><a href="#projects">Projetos</a></li>
-          <li><a href="#contact">Contato</a></li>
+          <li className='hover:text-gray-500'><a href="#about">Sobre</a></li>
+          <li className='hover:text-gray-500'><a href="#projects">Projetos</a></li>
+          <li className='hover:text-gray-500'><a href="#contact">Contato</a></li>
         </ul>
       </nav>
     </header>
   );
 };
-
 export default Header;
+
+
