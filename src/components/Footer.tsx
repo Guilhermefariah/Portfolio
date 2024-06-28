1fcce9bf49559646cd4fd9dfa9a49f60e54b5cc5
Guilhermefariah <guilhermefaria321@gmail.com>
2024-06-28 16:51:11 -0300

    header media query

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-black text-gray-200 p-4 text-center">
+    <footer className="bg-black text-gray-200 p-4 text-center fixed bottom-0 w-full">
       <p>&copy; {new Date().getFullYear()} Meu Portfólio. Todos os direitos reservados.</p>
     </footer>
   );
