9d42e896c0252daaeb3d27c993c25233e34f1b50
Guilhermefariah <guilhermefaria321@gmail.com>
2024-06-30 02:19:02 -0300

    ProjectCard animation

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -9,7 +9,7 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, gifUrl, visitorBadgeUrl }) => {
   return (
-    <div className="shadow-md flex flex-col sm:flex-row w-full items-center relative mt-44 mb-64">
+    <div className="shadow-md flex flex-col sm:flex-row w-full items-center relative mt-10 sm:mt-20 mb-20 sm:mb-32 transition-all duration-500 ease-in-out transform hover:scale-105">
       <div className="w-full p-4 text-left sm:text-center sm:w-1/2">
         <h2 className="text-6xl font-bold mb-4 text-transparent bg-gradient-to-r from-black via-white to-black bg-clip-text bg-no-repeat bg-[length:80%] animate-shine">
           {title}
@@ -17,10 +17,18 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, gifUrl, v
         <p className="text-4xl text-gray-200">{description}</p>
       </div>
 
-      <div className="sm:w-1/2 relative">
-        <img src={gifUrl} alt="GIF" className="w-11/12" />
-        <div className="absolute top-0 flex justify-center w-11/12">
-          <img src={visitorBadgeUrl} alt="Badge de Visitantes" className='w-full' />
+      <div className="sm:w-1/2 relative overflow-hidden">
+        <img
+          src={gifUrl}
+          alt="GIF"
+          className="w-11/12 transition-all duration-300 ease-in-out transform hover:scale-110"
+        />
+        <div className="absolute top-0 flex justify-center w-11/12 transition-all duration-500 ease-in-out transform hover:scale-105">
+          <img
+            src={visitorBadgeUrl}
+            alt="Badge de Visitantes"
+            className="w-full"
+          />
         </div>
       </div>
     </div>
