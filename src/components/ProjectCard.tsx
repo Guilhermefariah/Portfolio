38dc5b0e624ff59cd9eae47dd1fd1b0c0b75e14a
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-10 06:10:24 -0300

    remove div and add new projectCard

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 interface ProjectCardProps {
   title: string;
@@ -7,14 +8,30 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ title, description }) => {
   return (
-    <div className="flex items-center justify-center h-screen w-full relative transition-all duration-500 ease-in-out transform hover:scale-95">
+    <motion.div
+      className="flex items-center justify-center h-screen w-full relative"
+      whileHover={{ scale: 1.05 }}
+      transition={{ duration: 0.5 }}
+    >
       <div className="text-center w-full p-4 sm:w-3/4 lg:w-1/2">
-        <h2 className="text-4xl sm:text-5xl lg:text-6xl font-bold font-serif pb-4 text-transparent bg-gradient-to-r from-black via-white to-black bg-clip-text bg-no-repeat bg-[length:80%] animate-shine">
+        <motion.h2
+          className="text-4xl sm:text-5xl lg:text-6xl font-bold font-serif pb-4 text-transparent bg-gradient-to-r from-black via-white to-black bg-clip-text bg-no-repeat bg-[length:80%] animate-shine"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
           {title}
-        </h2>
-        <p className="text-xl sm:text-2xl lg:text-3xl text-gray-400 font-serif">{description}</p>
+        </motion.h2>
+        <motion.p
+          className="text-xl sm:text-2xl lg:text-3xl text-gray-400 font-serif"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          {description}
+        </motion.p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
