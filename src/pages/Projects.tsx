4778a35fadf0ad80f913ffa054fee63e30c68dff
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-14 20:23:32 -0300

    New project

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -6,41 +6,48 @@ const Projects: NextPage = () => {
   const projects = [
     {
       id: 1,
+      title: 'Zero Accident',
+      description: 'The Project is a panel to report problems in the city related to the lack of sidewalks and speed bumps. It aims to increase the safety of pedestrians and drivers, allowing citizens to report dangerous and unsafe locations.',
+      imageUrl: '/img/Zero_Accident.jpeg',
+      githubUrl: 'https://acidente-zero.vercel.app/',
+    },
+    {
+      id: 2,
       title: 'Landing-page',
       description: 'This project is an interactive and animated landing page built with Next.js, TypeScript, React and Tailwind CSS. It includes various components and features to provide a pleasant experience to users.',
       imageUrl: '/img/Landing-page.jpeg',
       githubUrl: 'https://freela-comercio.vercel.app/',
     },
     {
-      id: 2,
+      id: 3,
       title: 'SmartGrid',
       description: 'This is a front-end project for an e-commerce, developed in Next.js. It includes components for displaying products and a shopping cart.',
       imageUrl: '/img/SmartGrid.jpeg',
       githubUrl: 'https://lista-de-compra-opal.vercel.app/',
     },
     {
-      id: 3,
+      id: 4,
       title: 'Easy-Code-Roadmap',
       description: 'The aim is to provide an interactive and educational platform or roadmap for programming beginners and enthusiasts.',
       imageUrl: '/img/Easy-Code-Roadmap.png',
       githubUrl: 'https://easy-code-roadmap.vercel.app/',
     },
     {
-      id: 4,
+      id: 5,
       title: 'GuiDeveloper',
       description: 'Welcome to my developer page repository on GitHub! This project is a showcase of my work and projects. Here you can explore my skills and projects in HTML, CSS and JavaScript.',
       imageUrl: '/img/Guilhermefariah.png',
       githubUrl: 'https://guilhermefariah-github-io.vercel.app/',
     },
     {
-      id: 5,
+      id: 6,
       title: 'FaceForm',
       description: 'A project with a form developed in Next.js, React and TypeScript, with ESLint to ensure clean code and Tailwind CSS for easy styling. These tools provide a robust and efficient development experience.',
       imageUrl: '/img/FaceForm.png',
       githubUrl: 'https://guilhermefariah-github-io.vercel.app/',
     },
     {
-      id: 6,
+      id: 7,
       title: 'Shopping',
       description: 'This project is a simple shopping cart application developed using HTML, CSS, and JavaScript. The application allows users to add products to their cart, view the cart, and proceed to checkout.',
       imageUrl: '/img/ScreenShopping.jpeg',
@@ -61,7 +68,7 @@ const Projects: NextPage = () => {
                   <div className="absolute inset-0 bg-black font-serif bg-opacity-50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity duration-300">
                     <div className="bg-gray-200 opacity-90 p-2 rounded-lg shadow-lg text-center max-w-xs sm:max-w-sm md:max-w-md">
                       <h2 className="text-xl sm:text-2xl md:text-3xl font-serif">{project.title}</h2>
-                      <p className="text-sm sm:text-base md:text-lg text-black">{project.description}</p>
+                      <p className="text-sm sm:text-base md:text-lg text-black line-clamp-3">{project.description}</p>
                       <a href={project.githubUrl} target="_blank" rel="noopener noreferrer">
                         <button className="mt-4 bg-black text-gray-200 px-4 py-2 rounded-lg shadow-md hover:bg-blue-600 transition-colors duration-300">
                           View on Deploy
