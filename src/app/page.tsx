57ea25c0feb6df349e70b1d41788268c929e64f9
Guilhermefariah <guilhermefaria321@gmail.com>
2024-08-03 01:15:40 -0300

    refactor: improve Home component with best practices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer'
 import ProjectCard from '@/components/ProjectCard'
@@ -16,24 +15,21 @@ const Home: NextPage = () => {
 
   return (
     <div className={`flex flex-col min-h-screen bg-${bgColor}`}>
-      <Head>
-        <title>Meu Portfólio</title>
-        <meta name="description" content="Portfólio pessoal" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Header />
-      <div id="About" className={`flex-auto w-full justify-center items-center bg-${bgColor}`}>
-        <ProjectCard title="Guilherme Faria" description="Software Developer" />
-      </div>
-      <div id="Projects" className={`flex-1 flex justify-center items-center bg-${bgColor}`}>
-        <Projects />
-      </div>
-      <div id="Certificates" className={`flex-1 flex justify-center items-center bg-${bgColor}`}>
-        <Certificates />
-      </div>
-      <div id="Contact" className={`flex-1 flex justify-center items-center bg-${bgColor}`}>
-        <ProfileCard />
-      </div>
+      <main className="flex-auto w-full justify-center items-center">
+        <section id="About" className={`w-full justify-center items-center bg-${bgColor}`}>
+          <ProjectCard title="Guilherme Faria" description="Software Developer" />
+        </section>
+        <section id="Projects" className={`flex-1 flex justify-center items-center bg-${bgColor}`}>
+          <Projects />
+        </section>
+        <section id="Certificates" className={`flex-1 flex justify-center items-center bg-${bgColor}`}>
+          <Certificates />
+        </section>
+        <section id="Contact" className={`flex-1 flex justify-center items-center bg-${bgColor}`}>
+          <ProfileCard />
+        </section>
+      </main>
       <Footer />
       <ScrollToTopButton />
       <BackgroundButton bgColor={bgColor} BackgroundColor={BackgroundColor} />
